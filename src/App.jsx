3e8a0e99d9d7bdf43148da9596ba0fe996378ff1
Antra Verma <antraverma99@gmail.com>
2024-12-29 13:09:36 +0530

    - added option to generate password - added some basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 import { useState } from "react";
+import { RiLoopRightLine } from "react-icons/ri";
 import "./App.css";
 
 function App() {
-  const [password, setPassword] = useState("F&&jui45Ghsdujd");
+  const [password, setPassword] = useState("c[1CmPR2vS!<&L!c");
 
   const generateRandomPassword = () => {
-    
+    const charset =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+    const charsetLength = charset.length;
+
+    let newPassword = "";
+
+    const randomValues = new Uint32Array(16);
+
+    console.log({ randomValues });
+
+    window.crypto.getRandomValues(randomValues);
+
+    console.log({ randomValues });
+
+    for (let i = 0; i < 16; i++) {
+      const randomIndex = randomValues[i] % charsetLength;
+      newPassword += charset[randomIndex];
+    }
+
+    console.log({ newPassword });
+    setPassword(newPassword);
   };
 
   return (
-    <div>
-      <div className="input-wrapper">
-        <input value="it is going to be strong password" readOnly />
-        <button onClick={generateRandomPassword}>Re-generate</button>
-        <button>Copy</button>
+    <div className="container">
+      <div className="password-wrapper">
+        <input value={password} readOnly />
+        <button onClick={generateRandomPassword} className="regenerate-btn">
+          <RiLoopRightLine />
+        </button>
       </div>
+      <button className="copy-btn">Copy</button>
     </div>
   );
 }
